853f43e8771af4c1a0c5bc8a14c4788d77751a0f
Felipe Carvalho <felipe_pc7@hotmail.com>
2026-04-01 15:28:20 -0300

    refactor: removed user id from create workspace request DTO

--- src/main/java/com/pinhaDev/worknest/dto/request/CreateWorkspaceRequest.java
+++ src/main/java/com/pinhaDev/worknest/dto/request/CreateWorkspaceRequest.java
@@ -1,16 +1,10 @@
 package com.pinhaDev.worknest.dto.request;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-
-import java.util.UUID;
 
 public record CreateWorkspaceRequest(
         @NotEmpty
-        String name,
-
-        @NotNull
-        UUID userId
+        String name
 ) {
 
 }
