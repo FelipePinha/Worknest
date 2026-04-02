70736299f48060703b6ecb7741619796114ce0f7
Felipe Carvalho <felipe_pc7@hotmail.com>
2026-04-02 16:33:07 -0300

    refactor: add JdbcTypeCode to fix enum error

--- src/main/java/com/pinhaDev/worknest/domain/models/Task.java
+++ src/main/java/com/pinhaDev/worknest/domain/models/Task.java
@@ -1,8 +1,11 @@
 package com.pinhaDev.worknest.domain.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.pinhaDev.worknest.domain.enums.TaskStatus;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -26,12 +29,14 @@ public class Task {
     @Column(columnDefinition = "TEXT")
     private String description;
 
-    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Column(nullable = false)
     private TaskStatus status;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "workspace_id")
+    @JsonIgnore
     private Workspace workspace;
 
     @Column(name = "created_at", nullable = false, updatable = false)
